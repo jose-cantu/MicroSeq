b5f5b760b424723e8b6ac2093a40302561b7718d
JasonSilverlake <josecantu2314@gmail.com>
2025-05-12 10:54:39 -0500

    updated add_taxonomy so correct sseqid normalisation and dorp version suffix so now all 3 databases work when using add_taxonomy

--- src/microseq_tests/utility/add_taxonomy.py
+++ src/microseq_tests/utility/add_taxonomy.py
@@ -36,7 +36,14 @@ def run_taxonomy_join(hits_fp: Path, taxonomy_fp: Path, out_fp: Path) -> None:
 
     sep = "\t" if hits_fp.suffix.lower() == ".tsv" else None 
     hits = pd.read_csv(normalise_tsv(hits_fp), sep=sep, engine="python", header=None, names=COLS,).rename(columns={"qseqid": "sample_id"}) 
-    
+   # canonicalize the subject ID so it can match the taxonomy table for each of the databases 
+    hits["sseqid"] = ( 
+       hits["sseqid"].astype(str)
+           .str.rstrip("|") 
+           .str.split("|").str[-1] # drop gi|.....|ref| or silva|..... 
+           .str.split(" ").str[0] # drop everything after the first space
+           .str.replace(r"\.\d+\.\d+$", "", regex=True)   # Silva special case           
+                      ) 
     tax = (
         pd.read_csv(normalise_tsv(taxonomy_fp), sep="\t")                # original columns
             .rename(                                        # normalise names
